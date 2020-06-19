512648e792608b3407b70c58bb70d79415606300
Evans Marema <kenmarema@gmail.com>
2020-06-19 14:03:33 +0200

    version pf base app the works with pipeline

---
+++ test_app.py
@@ -0,0 +1,65 @@
+'''
+This is a version of base_app that in a pipeline that vectorizes 
+and fits the data to the model the pipeline wwas build with.
+'''
+
+# Streamlit dependencies
+import streamlit as st
+import joblib,os
+
+# Data dependencies
+import pandas as pd
+
+
+raw = pd.read_csv('Kaggle_resources/datasets/train.csv')
+
+# The main function where we will build the actual app
+def main():
+	"""Tweet Classifier App with Streamlit """
+
+	# Creates a main title and subheader on your page -
+	# these are static across all pages
+	st.title("Tweet Classifer")
+	st.subheader("Climate change tweet classification")
+
+	# Creating sidebar with selection box -
+	# you can create multiple pages this way
+	options = ["Prediction", "Information"]
+	selection = st.sidebar.selectbox("Choose Option", options)
+
+	# Building out the "Information" page
+	if selection == "Information":
+		st.info("General Information")
+		# You can read a markdown file from supporting resources folder
+		st.markdown("Some information here")
+
+		st.subheader("Raw Twitter data and label")
+		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
+			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+
+	# Building out the predication page
+	if selection == "Prediction":
+		st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
+
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			### vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+            
+			path = 'Kaggle_resources/Models'
+
+			predictor = joblib.load(open(os.path.join(path,"LSVC_01.pkl"),"rb"))
+			prediction = predictor.predict([tweet_text])
+
+			# When model has successfully run, will print prediction
+			# You can use a dictionary or similar structure to make this output
+			# more human interpretable.
+			st.success("Text Categorized as: {}".format(prediction))
+
+
+# Required to let Streamlit instantiate our web app.  
+if __name__ == '__main__':
+	main()
