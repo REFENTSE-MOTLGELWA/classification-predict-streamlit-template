fda89b9e1923b096b961d97e8ecd743ddae6b35c
Sandile-Dladla <sdladlad@gmail.com>
2020-06-26 08:19:46 +0200

    added more pages to navigate on

--- test_app2.py
+++ test_app2.py
@@ -30,117 +30,102 @@ def main():
 	#st.subheader("Climate change tweet classification")
 
 
-	# Creating predictions side bar
-	model_page = ["Home", "Lsvc", "Knn"]
-	st.sidebar.title("PREDICTIONS")
-	selection = st.sidebar.radio("Choose Model", model_page)
+	# Creating side bar
+	navigation = ["HOME", "PREDICTIONS", "INSIGHTS", "EDA", "ABOUT TEAM", "READMEfile"]
+	st.sidebar.title("NAVIGATION")
+	selection = st.sidebar.radio("make your selection",navigation)
 
-	if selection == 'Home':
-		st.info("Welcome to climate change sentiment analysis")
-		st.info("""Please select a model under PREDICTIONS to predict a tweet 
-		or make a selection under INFORMATION to get more information about the site, the exploratory data analysis and preprocessing.""")
+	# For selecting side bars
+	if selection == 'HOME':
+		st.markdown(open("resource2/info.md").read())
+	
 
 		
 	# Building out the predication page
-
-	#Linear Support Vector classifier
-	if selection == "Lsvc":
-		st.info("Give brief info about Lsvc model")
-		# Creating a text box for user input
+	elif selection == "PREDICTIONS":
+		#Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
-		
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			### vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-            
-			path = 'resource2/Models'
+		#List of models
+		model_list = ["LSVC", "KNN"]
+		#Model selection
+		model_select = st.selectbox("Choose Model", model_list)
 
-			predictor = joblib.load(open(os.path.join(path,"LSVC_01.pkl"),"rb"))
-			prediction = predictor.predict([tweet_text])
-			if prediction == [1]:
-				results = 'Pro'
-			elif prediction == [-1]:
-				results = 'Anti'
-			elif prediction == [2]:
-				results = 'News'
-			elif prediction == [0]:
-				results = 'Neutral'
+		#Linear Support Vector classifier
+		if model_select == "LSVC":
+			#What is the model about
+			st.info("Give brief info about Linear Support Vector Classifier model")
+			#
+			if st.button("Classify"):
+				# Transforming user input with vectorizer
+				### vect_text = tweet_cv.transform([tweet_text]).toarray()
+				# Load your .pkl file with the model of your choice + make predictions
+				# Try loading in multiple models to give the user a choice
+				
+				path = 'resource2/Models'
 
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as:   {}".format(results))
+				predictor = joblib.load(open(os.path.join(path,"LSVC_01.pkl"),"rb"))
+				#change prediction out to display in more human interpretable
+				prediction = predictor.predict([tweet_text])
+				if prediction == [1]:
+					results = 'Pro'
+				elif prediction == [-1]:
+					results = 'Anti'
+				elif prediction == [2]:
+					results = 'News'
+				elif prediction == [0]:
+					results = 'Neutral'
+				#prediction output
+				st.success("Text Categorized as:   {}".format(results))
 
-	#K Nearest Neighbor
-	elif selection == 'Knn':
-		st.info("Give brief info about Knn model")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+		#K Nearest Neighbor
+		elif model_select == 'KNN':
+			st.info("Give brief info about K Nearest Neighbor model")
 
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			### vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-            
-			path = 'resource2/Models'
+			if st.button("Classify"):
+				# Transforming user input with vectorizer
+				### vect_text = tweet_cv.transform([tweet_text]).toarray()
+				# Load your .pkl file with the model of your choice + make predictions
+				# Try loading in multiple models to give the user a choice
+				
+				path = 'resource2/Models'
 
-			predictor = joblib.load(open(os.path.join(path,"KNN_01.pkl"),"rb"))
-			prediction = predictor.predict([tweet_text])
+				predictor = joblib.load(open(os.path.join(path,"KNN_01.pkl"),"rb"))
+				prediction = predictor.predict([tweet_text])
+				#change prediction out to display in more human interpretable 
+				if prediction == [1]:
+					results = 'Pro'
+				if prediction == [-1]:
+					results = 'Anti'
+				if prediction == [2]:
+					results = 'News'
+				if prediction == [0]:
+					results = 'Neutral'
+				#prediction output	
+				st.success("Text Categorized as: {}".format(results))
+		st.subheader("Meaning of different output")
+		st.markdown("Pro - the tweet supports the belief of man-made climate change")
+		st.markdown("Anti - the tweet does not believe in man-made climate change")
+		st.markdown("Neutral - the tweet neither supports nor refutes the belief of man-made climate change")
+		st.markdown("News - the tweet links to factual news about climate change")
 
-			if prediction == [1]:
-				results = 'Pro'
-			if prediction == [-1]:
-				results = 'Anti'
-			if prediction == [2]:
-				results = 'News'
-			if prediction == [0]:
-				results = 'Neutral'
-
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(results))
-
-
-
+	
 	# Building out the "Information" page
-	st.sidebar.title("INFORMATION")
-	options = ["General", "Readme", "EDA", "Preprocessing"]
-	select_info = st.sidebar.radio("Choose Option", options)
-	# Building out the "Information" page
-	if select_info == "General":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+	elif selection == "INSIGHTS":
+		st.info("Our insights goes here")
 
+	elif selection == "EDA":
+		st.markdown("Display the EDA here")
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
+	elif selection == "ABOUT TEAM":
+		#Display info about team 
+		st.markdown("Display team info here")
 
-	if select_info == "Readme":
-		st.info("README")
-		# You can read a markdown file from supporting resources folder
+	elif selection == "READMEfile":
+		#Display readme page
 		st.markdown(open("README.md").read())
-
-		
-
-	if select_info == "EDA":
-		st.info("EDA")
-		#our_eda = eda.explore_data()
-		#st.success(our_eda)
-		st.markdown("Display the EDA here")
-
-
-
-	if select_info == "Preprocessing":
-		st.info("Preprocessing")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Display the PREPROCESSING here")
-
 
 		
 # Required to let Streamlit instantiate our web app.  
