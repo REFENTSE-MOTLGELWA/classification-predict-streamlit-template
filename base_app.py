068fcdc5636a6f4e65549cb2471f0a633db4624e
Sandile-Dladla <sdladlad@gmail.com>
2020-06-29 12:17:55 +0200

    renamed test_app2.py and resource2.py to base_app.py and resources.py and deleted the duplicate

--- base_app.py
+++ base_app.py
@@ -1,39 +1,33 @@
-"""
+'''
+This is a version of base_app that in a pipeline that vectorizes 
+and fits the data to the model the pipeline wwas build with.
+'''
 
-    Simple Streamlit webserver application for serving developed classification
-	models.
-
-    Author: Explore Data Science Academy.
-
-    Note:
-    ---------------------------------------------------------------------
-    Plase follow the instructions provided within the README.md file
-    located within this directory for guidance on how to use this script
-    correctly.
-    ---------------------------------------------------------------------
-
-    Description: This file is used to launch a minimal streamlit web
-	application. You are expected to extend the functionality of this script
-	as part of your predict project.
-
-	For further help with the Streamlit framework, see:
-
-	https://docs.streamlit.io/en/latest/
-
-"""
 # Streamlit dependencies
 import streamlit as st
 import joblib,os
-
-# Data dependencies
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import TreebankWordTokenizer
+from nltk import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+#import spacy
+#from PIL import Image
 
-# Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
-tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
-# Load your raw data
-raw = pd.read_csv("resources/train.csv")
+df_test = pd.read_csv('resources/datasets/test.csv')
+test_df = df_test.set_index('tweetid')
+df_train = pd.read_csv('resources/datasets/train.csv')
+train_df = df_train.set_index('tweetid')
+ 
+
+
 
 # The main function where we will build the actual app
 def main():
@@ -41,43 +35,165 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
+	img = "resources/images/logo.jpg"
+	st.image(img, caption="It is what it is")
 	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
 
-	# Creating sidebar with selection box -
-	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+	# Creating side bar for navigation
+	navigation = ["HOME", "PREDICTIONS", "EDA", "INSIGHTS", "ABOUT TEAM", "READMEfile"]
+	st.sidebar.title("NAVIGATION")
+	selection = st.sidebar.radio(" ",navigation)
 
-	# Building out the "Information" page
-	if selection == "Information":
-		st.info("General Information")
-		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
 
+	# For selecting side bars
+	if selection == 'HOME':
+		st.markdown(open("resources/info.md").read())
 		st.subheader("Raw Twitter data and label")
+		
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
-			st.write(raw[['sentiment', 'message']]) # will write the df to the page
+			values = st.slider("Slide to the right to select and view quantity",0, 16000)
+			st.write(df_train[0:values])
+
+			sub_data = ["Train Data Shape", "Test Data Shape"]
+			data_select = st.selectbox("View Data Shape", sub_data)
+
+			if data_select == "Train Data Shape":	
+				st.write(df_train.shape)
+
+			elif data_select == "Test Data Shape":
+				st.write(df_test.shape)
+	
 
 	# Building out the predication page
-	if selection == "Prediction":
-		st.info("Prediction with ML Models")
-		# Creating a text box for user input
-		tweet_text = st.text_area("Enter Text","Type Here")
+	elif selection == "PREDICTIONS":
+		#Creating a text box for user input
+		tweet_text = st.text_area("Enter some text below:","")
+		#List of models
+		model_list = ["LSVC", "KNN"]
+		#Model selection drop down
+		model_select = st.selectbox("Choose Model:", model_list)
 
-		if st.button("Classify"):
-			# Transforming user input with vectorizer
-			vect_text = tweet_cv.transform([tweet_text]).toarray()
-			# Load your .pkl file with the model of your choice + make predictions
-			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
-			prediction = predictor.predict(vect_text)
+		#Linear Support Vector classifier
+		if model_select == "LSVC":
+			#What is the model about
+			st.markdown("replace text with description of Linear Support Vector Classifier model")
+			#
+			if st.button("Classify"):
+				# Transforming user input with vectorizer
+				### vect_text = tweet_cv.transform([tweet_text]).toarray()
+				# Load your .pkl file with the model of your choice + make predictions
+				# Try loading in multiple models to give the user a choice
+				
+				path = 'resources/models'
 
-			# When model has successfully run, will print prediction
-			# You can use a dictionary or similar structure to make this output
-			# more human interpretable.
-			st.success("Text Categorized as: {}".format(prediction))
+				predictor = joblib.load(open(os.path.join(path,"LSVC_01.pkl"),"rb"))
+				#change prediction out to display in more human interpretable
+				prediction = predictor.predict([tweet_text])
+				if prediction == [1]:
+					results = 'Pro'
+				elif prediction == [-1]:
+					results = 'Anti'
+				elif prediction == [2]:
+					results = 'News'
+				elif prediction == [0]:
+					results = 'Neutral'
+				#prediction output
+				st.success("Text Categorized as:   {}".format(results))
 
+		#K Nearest Neighbor
+		elif model_select == 'KNN':
+			st.markdown("replace text with description of K Nearest Neighbor model")
+
+			if st.button("Classify"):
+				# Transforming user input with vectorizer
+				### vect_text = tweet_cv.transform([tweet_text]).toarray()
+				# Load your .pkl file with the model of your choice + make predictions
+				# Try loading in multiple models to give the user a choice
+				
+				path = 'resources/models'
+
+				predictor = joblib.load(open(os.path.join(path,"KNN_01.pkl"),"rb"))
+				prediction = predictor.predict([tweet_text])
+				#change prediction out to display in more human interpretable 
+				if prediction == [1]:
+					results = 'Pro'
+				if prediction == [-1]:
+					results = 'Anti'
+				if prediction == [2]:
+					results = 'News'
+				if prediction == [0]:
+					results = 'Neutral'
+				#prediction output	
+				st.success("Text Categorized as: {}".format(results))
+		st.subheader("Meaning of different categories")
+		st.info("Pro - the tweet supports the belief of man-made climate change")
+		st.info("Anti - the tweet does not believe in man-made climate change")
+		st.info("Neutral - the tweet neither supports nor refutes the belief of man-made climate change")
+		st.info("News - the tweet links to factual news about climate change")
+
+	
+	# Building out the "Information" paga
+		
+	elif selection == "EDA":
+		st.subheader("Exploratry Data Analysis")
+		
+		if st.checkbox('Display sentiment distribution'): # data is hidden if box is unchecked
+			# Count of classes in sentiment 
+			plt.figure(figsize=(10, 6))
+			sns.set(style="darkgrid")
+			ax = sns.countplot(x='sentiment', data=train_df)
+			ax.set_title("No. of tweets per sentiment")
+			st.pyplot()
+
+			# train_df['length'] = train_df['message'].apply(len)
+			# g = sns.FacetGrid(data=train_df, col='sentiment', col_wrap=4)
+			# g.map(plt.hist, 'length', bins = 20, color = 'g')
+			# st.pyplot()
+
+			df_train['length'] = df_train['message'].apply(len)
+			graph = sns.FacetGrid(data=df_train, col = 'sentiment', 
+								col_wrap=2, height=3, aspect=2)
+			graph.map(plt.hist, 'length', bins = 30, color = 'g')
+			st.pyplot()
+
+		elif st.checkbox("View the word cloud"):
+			# Create and generate a word cloud image
+			fig, ax = plt.subplots(figsize=(15, 15))
+			tweet = ''.join(tweet for tweet in train_df['message'])
+			wordcloud = WordCloud(max_font_size=50, max_words=100,
+									background_color='white').generate(tweet)
+			# Displaying the images
+			ax.imshow(wordcloud, interpolation='bilinear')
+			ax.set_title('Word cloud for all sentiments', fontsize=20)
+			ax.axis('off')
+			st.pyplot()
+
+
+
+	elif selection == "INSIGHTS":
+		st.info("Our insights goes here")
+		news_class = round((train_df.sentiment.value_counts()[2]/len(train_df))*100,2)
+		pro_class = round((train_df.sentiment.value_counts()[1]/len(train_df))*100,2)
+		neutral_class = round((train_df.sentiment.value_counts()[0]/len(train_df))*100,2)
+		anti_class = round((train_df.sentiment.value_counts()[-1]/len(train_df))*100,2)
+
+		data = [news_class,pro_class, neutral_class, anti_class]
+		classes = 'News','Pro','Neutral','Anti'
+		my_colors = ['lightblue','lightyellow','pink', 'violet']
+		plt.pie(data,labels=classes,autopct='%1.1f%%', colors=my_colors)
+		plt.title('Sentiment distribution in train data')
+		plt.axis('equal')
+		st.pyplot()
+
+	elif selection == "ABOUT TEAM":
+		#Display info about team 
+		st.markdown(open("team.md").read())
+
+	elif selection == "READMEfile":
+		#Display readme page
+		st.markdown(open("README.md").read())
+
+		
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
--- team.md
+++ team.md
@@ -6,14 +6,14 @@
 
 
 ## Evans Marema - Team Leader
-![Evans Marema](resource2/images/logo.jpg)
+![Evans Marema](resources/images/Evans.jpg)
 
 The central processing unit - Ensures the different application of system work well together. 
 The engine that ensures the the phone is up and running
 
 
 ## Vinita Maharaj
-![Vinita Maharaj]("resource2/images/V.jpg")
+![Vinita Maharaj]("resources/images/V.jpg")
 
 
 The RAM - Ensures the system info is available at finger tips. 
@@ -21,28 +21,28 @@ Data is needed right on time otherwise the sytem will collapse
 
 
 ## Gugu Mtonjeni
-![Gugu Mtonjeni](resource2/images/Gugu.jpg)
+![Gugu Mtonjeni](resources/images/Gugu.jpg)
 
 The Display - Screen that ensures you see whats in the phone
 It always good to see the end results being displayed beautifully
 
 
 ## Refentse Motlogelwa
-![Refentse Motlogelwa](resource2/images/logo.jpg)
+![Refentse Motlogelwa](resources/images/Refentse.jpg)
 
 The Anti Virus - Never try to snick in viruses on the phone, he will catch them on the spot.
 Will throw away those repeated tweets wich slows down the system
 
 
 ## Sandile Dladla
-![Sandile Dlada](resource2/images/logo.jpg)
+![Sandile Dlada](resources/images/SandileD.jpg)
 
 Navigator - Allows access to different part of the phone.
 Moving from one part to another
 
 
 ## Sandile Mkize
-![Sandile Mkize](resource2/images/logo.jpg)
+![Sandile Mkize](resources/images/SandileM.jpg)
 
 The mother board - If the circuit borad wasn't there, how will all the other components interact with each other.
 Will explore data given for info and try to make sense of it
