b596402ed27d1fcc56c2c3dbad8691ad7ee3b0ca
Sandile-Dladla <sdladlad@gmail.com>
2020-06-29 17:14:25 +0200

    changes

--- base_app.py
+++ base_app.py
@@ -99,7 +99,7 @@ def main():
 				#prediction output
 				st.success("Text Categorized as:   {}".format(results))
 
-		#K Nearest Neighbor
+		#K Nearest Neighbor Model
 		elif model_select == 'KNN':
 			st.markdown("KNN works by finding the distances between a query and all the examples in the data, selecting the specified number examples (K) closest to the query, then votes for the most frequent label (in the case of classification) or averages the labels (in the case of regression).")
 
